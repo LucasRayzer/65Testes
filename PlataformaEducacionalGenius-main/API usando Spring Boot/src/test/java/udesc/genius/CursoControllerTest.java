61a68e04cac830d0adc85a3210b34b63b192dd23
Lucas Pietro Biasi Rayzer <145388833+LucasRayzer@users.noreply.github.com>
2024-11-23 15:48:12 -0300

    Update

--- PlataformaEducacionalGenius-main/API usando Spring Boot/src/test/java/udesc/genius/CursoControllerTest.java
+++ PlataformaEducacionalGenius-main/API usando Spring Boot/src/test/java/udesc/genius/CursoControllerTest.java
@@ -39,43 +39,35 @@ public class CursoControllerTest {
         cursoModel.setIdCurso(UUID.randomUUID());
     }
 
-    // Teste CT10 - Criar curso com dados válidos
+    //CT10 passa
     @Test
     void testCriarCursoComDadosValidos() {
-        // Simulando o retorno do repositório
+       
         when(cursoRepository.save(any(CursoModel.class))).thenReturn(cursoModel);
 
-        // Chamando o método salvarCurso do controller
         ResponseEntity<Object> response = cursoController.salvarCurso(cursoRecordDto);
 
-        // Verificando se o código de status é 201 (Created)
         assertEquals(201, response.getStatusCodeValue());
 
-        // Verificando se o corpo da resposta não é nulo e contém o curso salvo
         assertNotNull(response.getBody());
         assertEquals(cursoModel, response.getBody());
         
-        // Verificando se o método save foi chamado uma vez no repositório
         verify(cursoRepository, times(1)).save(any(CursoModel.class));
     }
 
-    // Teste CT11 - Criar curso com nome vazio
+    //CT11 não passa pois não possuí validação no back
     @Test
     void testCriarCursoComNomeVazio() {
-        // Curso com nome vazio
+
         CursoRecordDto cursoComNomeVazio = new CursoRecordDto(" ", "Curso de graduação");
 
-        // Chamando o método salvarCurso do controller
         ResponseEntity<Object> response = cursoController.salvarCurso(cursoComNomeVazio);
 
-        // Verificando se o código de status é 400 (Bad Request), pois o nome é inválido
         assertEquals(400, response.getStatusCodeValue());
 
-        // Verificando se o corpo da resposta contém a mensagem de erro esperada
         assertNotNull(response.getBody());
         assertEquals("Nome inválido - curso não criado.", response.getBody());
 
-        // Verificando se o método save não foi chamado no repositório
         verify(cursoRepository, times(0)).save(any(CursoModel.class));
     }
 }
